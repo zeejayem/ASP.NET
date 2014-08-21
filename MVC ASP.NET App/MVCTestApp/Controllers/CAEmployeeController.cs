4d5709fb874f7b40a76c4403eda4a4e45a7ec80a
Zarah <zarah.mcintosh@gmail.com>
2014-08-20 17:41:12 -0700

    MVC ASP.NET App
    MVC in Visual Studio

---
+++ MVC ASP.NET App/MVCTestApp/Controllers/CAEmployeeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTestApp.Models;//this gives access to the models
+
+namespace MVCTestApp.Controllers
+{
+    public class CAEmployeeController : Controller
+    {
+        //
+        // GET: /CAEmployee/
+
+        public ActionResult Index()
+        {
+            CommunityAssistEntities cae = new CommunityAssistEntities();//the code below is linq
+            var employees = from e in cae.EmployeeJobTitles//when trying to do links it's best to start with farthest out and work in.
+                            orderby e.Employee.Person.PersonLastName
+                            select new
+                            {
+                                e.Employee.Person.PersonLastName,
+                                e.Employee.Person.PersonFirstName,
+                                e.Employee.Person.PersonUsername,
+                                e.Employee.EmployeeHireDate,
+                                e.Jobtitle.JobTitleName,
+                                e.Employee.EmployeeStatus
+                            };
+            List<CAEmployee> employeeList = new List<CAEmployee>();
+            foreach (var x in employees)
+            {
+                CAEmployee emp = new CAEmployee();
+                emp.LastName = x.PersonFirstName;
+                emp.FirstName = x.PersonFirstName;
+                emp.Email = x.PersonUsername;
+                emp.HireDate = x.EmployeeHireDate.ToString();
+                emp.JobTitle = x.JobTitleName;
+                emp.status = x.EmployeeStatus;
+
+                employeeList.Add(emp);
+            }
+            return View(employeeList);
+        }
+
+    }
+
+    public class CAEmployee//this is to store the combination, holding the data.
+    {
+        public string LastName { set; get; }
+        public string FirstName { set; get; }
+        public string Email { set; get; }
+        public string HireDate { set; get; }
+        public string JobTitle { set; get; }
+        public string status { set; get; }
+
+    }
+}
---
+++ MVC ASP.NET App/MVCTestApp/Controllers/ServicesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCTestApp.Models;
+
+namespace MVCTestApp.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private CommunityAssistEntities db = new CommunityAssistEntities();
+
+        //
+        // GET: /Services/
+
+        public ActionResult Index()
+        {
+            return View(db.CommunityServices.ToList());
+        }
+
+        //
+        // GET: /Services/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            CommunityService communityservice = db.CommunityServices.Find(id);
+            if (communityservice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(communityservice);
+        }
+
+        //
+        // GET: /Services/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Services/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CommunityService communityservice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CommunityServices.Add(communityservice);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(communityservice);
+        }
+
+        //
+        // GET: /Services/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            CommunityService communityservice = db.CommunityServices.Find(id);
+            if (communityservice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(communityservice);
+        }
+
+        //
+        // POST: /Services/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CommunityService communityservice)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(communityservice).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(communityservice);
+        }
+
+        //
+        // GET: /Services/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            CommunityService communityservice = db.CommunityServices.Find(id);
+            if (communityservice == null)
+            {
+                return HttpNotFound();
+            }
+            return View(communityservice);
+        }
+
+        //
+        // POST: /Services/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CommunityService communityservice = db.CommunityServices.Find(id);
+            db.CommunityServices.Remove(communityservice);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
